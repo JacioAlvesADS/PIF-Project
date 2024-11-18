d020a81a668a6923b2167efe216c10f4590b899b
Jácio Alves <163608767+JacioAlvesADS@users.noreply.github.com>
2024-11-18 13:31:20 -0300

    Botao de Iniciar + Novos Meteoros

--- src/main.c
+++ src/main.c
@@ -15,16 +15,19 @@
 
 
 int x = 34, y = 5;
+int x_dificil = 34, y_dificil = 5;
 int incX = 1, incY = 1;
 int incNave = 1;
 int x_nave = 32, y_nave = 17;
 int x_pontuacao = 0, y_pontuacao = 0;
-int x_time = MAXX - 50, y_time = 32;
+int x_time = MAXX - 45, y_time = 32;
 time_t startTime;
 double velocidade = 50;
+char continuar;
 
 
-int time_decorrido = 0;
+
+int long long time_decorrido = 0;
 
 /*void printHello(int nextX, int nextY)
 {
@@ -45,13 +48,9 @@ void displayTimer() {
     int elapsedTime = (int)(currentTime - startTime);
 
 
-    int minutes = elapsedTime / 60;
-    int seconds = elapsedTime % 60;
-
     screenGotoxy(x_time, y_time);
-    printf("Tempo decorrido: %02d:%02d", minutes, seconds);
-    fflush(stdout);
-    int time_decorrido = time_decorrido + 1;
+    printf("Pontuação: %lld", time_decorrido);
+    //int time_decorrido = time_decorrido + 10;
 }
 
 void pontuacao_total(int vida){
@@ -73,6 +72,24 @@ void meteoros(int nextX, int nextY){
     y = nextY;
     screenGotoxy(x, y);
     printf("0");
+}
+
+void meteoros_dificil(int meteoroX, int meteoroY){
+    screenSetColor(BLUE, DARKGRAY);
+    screenGotoxy(x_dificil, y_dificil);
+    printf(" ");
+    x_dificil = meteoroX;
+    y_dificil = meteoroY;
+    screenGotoxy(x_dificil, y_dificil);
+    printf("0");
+}
+void comecar(int x_comecar, int y_comecar){
+    screenSetColor(RED, DARKGRAY);
+    screenGotoxy(x_comecar, y_comecar);
+    //printf("              ");
+    screenGotoxy(x_comecar, y_comecar);
+    printf("Começar Jogo!\n");
+    printf("                              Aperte 's' para começar\n");
 }
 
 void nave(int naveX){
@@ -116,10 +133,21 @@ int main()
     meteoros(x, y);
     screenUpdate();
     int pontuacao = 100;
+    //char continuar;
+    //comecar(32, 15);
 
+    while(continuar != 's'){
+        comecar(34,5);
+        scanf("%c", &continuar);
+    }
+    screenGotoxy(34,5);
+    printf("                        \n");
+    screenGotoxy(30,6);
+    printf("                           ");
 
     while (pontuacao != 0) //enter
     {
+        //comecar(1000,1000);
         // Handle user input
         if (keyhit()) 
         {
@@ -134,7 +162,10 @@ int main()
             int newX;
             int newY = y + incY;
             int naveEixo = x_nave;
-
+            int meteoroPositionX;
+            int meteoroPositionY = y_dificil + incY;
+            int positionBotton = x + incY;
+            int positionBotton_y;
             
 
             if (newY >= MAXY){
@@ -152,7 +183,22 @@ int main()
             if (y == y_nave && x >= x_nave && x <= x_nave + 4){
                 newX = (rand() % (MAXX - 4)) + 3;
                 newY = MINY + 3;//Colidir o Meteoro com a nave.
+                velocidade = 50;
                 pontuacao = pontuacao - 10;
+                
+            }
+            if (time_decorrido >= 600){
+                if (meteoroPositionY >= MAXY){
+                    meteoroPositionX = (rand() % (MAXX - 4)) + 3; //Movimentação do Meteoro
+                    meteoroPositionY = MINY + 3;
+                    }
+                if (y_dificil == y_nave && x_dificil >= x_nave && x_dificil <= x_nave + 4){
+                    meteoroPositionX = (rand() % (MAXX - 4)) + 3;
+                    meteoroPositionY = MINY + 3;//Colidir o Meteoro com a nave.
+                    pontuacao = pontuacao - 10;
+                
+            }
+
             }
                  
             switch (time_decorrido) {
@@ -160,7 +206,7 @@ int main()
                     velocidade = 40;
                     timerInit(velocidade);
                     break;
-                case 600:  
+                case 600:
                     velocidade = 35;
                     timerInit(velocidade);
                     break;
@@ -172,7 +218,7 @@ int main()
                     break;
             }
 
-            time_decorrido++;
+            time_decorrido = time_decorrido + 1;
             //printf("%d", time_decorrido);
             //x_nave = naveEixo;
 
@@ -182,12 +228,14 @@ int main()
             displayTimer();
             meteoros(newX, newY);
             nave(naveEixo);
+            meteoros_dificil(meteoroPositionX, meteoroPositionY);
             pontuacao_total(pontuacao);
             //printHello(newX, newY);
 
             screenUpdate();
         }
     }
+    
 
     keyboardDestroy();
     screenDestroy();
